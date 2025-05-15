09e4675380d4fafed36cff67e1402ef959176557
Gordon Blasco <gordonblasco@gmail.com>
2025-05-15 13:16:14 -0700

    working well. Pre NChad swap

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,24 +1,29 @@
 -- ~/dotfiles/hammerspoon/init.lua
 
 -- Convenience modifier
-local hyper = {"ctrl", "alt", "shift", "cmd"}
+local hyper = { "ctrl", "alt", "shift", "cmd" }
 
 -- Hotkey to program mapping:
 local appHotkeys = {
-    O = "Obsidian",
-    U = "Google Chrome",
-    I = "Visual Studio Code",
-    P = "Microsoft OneNote",
-    Q = "Microsoft Teams",
-    W = "Microsoft Outlook",
-    E = "Slack",
+	O = "Obsidian",
+	U = "Google Chrome",
+	I = "Visual Studio Code",
+	P = "Microsoft OneNote",
+	Q = "Microsoft Teams",
+	W = "Microsoft Outlook",
+	E = "Slack",
 }
 
 for key, app in pairs(appHotkeys) do
-    hs.hotkey.bind(hyper, key, function()
-        hs.application.launchOrFocus(app)
-    end)
+	hs.hotkey.bind(hyper, key, function()
+		hs.application.launchOrFocus(app)
+	end)
 end
+
+-- bulkier version
+-- hs.hotkey.bind(hyper, "H", function()
+--   hs.window.layout.move(1, [0,0,50,100], 0, 0)
+-- end)
 
 -- bulkier version
 -- hs.hotkey.bind(hyper, "K", function()
@@ -26,7 +31,7 @@ end
 -- end)
 
 hs.hotkey.bind(hyper, "R", function()
-    hs.reload()
+	hs.reload()
 end)
 
 -- Confirm it loaded
