7926ad1052677b28003de7658dbd2f5cb7ec0739
Gordon Blasco <gordonblasco@gmail.com>
2025-05-14 18:36:11 -0700

    clearn yup

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,13 @@
 return {
+  -- add themes
   { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+      --colorscheme = "gruvbox-material",
     },
   },
 }
