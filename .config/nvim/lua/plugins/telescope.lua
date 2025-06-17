5a0f33350368d5a7960a646211c1670992eb76aa
Gordon Blasco <gordonblasco@gmail.com>
2025-06-17 10:50:04 -0700

    added telescope hidden defaults

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    pickers = {
+      find_files = {
+        hidden = true,
+        no_ignore = true,
+        no_ignore_parent = true,
+      },
+    },
+  },
+}
